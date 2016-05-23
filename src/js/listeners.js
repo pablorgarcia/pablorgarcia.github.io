039174d97cf492b99a0549b89168574cb62e4550
PableraShow <pablo.ramon.garcia.fernandez@gmail.com>
2016-05-23 19:53:21 -0300

    about-me link in progress

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,6 +1,18 @@
 
+// The init function do the animation control 
 document.addEventListener('DOMContentLoaded', init, false);
 
-for(var i=0; i<2; i++) {
+// Firsts LinkedIn, E-mail and About me links
+for(var i=0; i<3; i++) {
   anchor[i].addEventListener('click', linkCliked, false);
 }
+
+// About text link
+anchor[2].addEventListener('click', internalURL, false);
+/*
+http://stackoverflow.com/questions/3498492/javascriptvoid0-vs-return-false-vs-preventdefault
+http://lea.verou.me/2011/05/change-url-hash-without-page-jump/
+*/
+
+
+//about_text.addEventListener('click', internalURL, false);
