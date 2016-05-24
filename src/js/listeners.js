75fafb435f22667e7eeda732241548eedb013094
PableraShow <pablo.ramon.garcia.fernandez@gmail.com>
2016-05-24 19:33:28 -0300

    working on about me button, analytics commented

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -1,24 +1,20 @@
 
-// The init function do the animation control 
+// The init function to do the animation control
 document.addEventListener('DOMContentLoaded', init, false);
 
 // Firsts LinkedIn, E-mail and About me links
-for(var i=0; i<3; i++) {
-  anchor[i].addEventListener('click', linkCliked, false);
-}
+for(var i=0; i<3; i++) { anchor[i].addEventListener('click', linkCliked, false); }
 
-// About text link
-anchor[2].addEventListener('click', internalURL, false);
-/*
-http://stackoverflow.com/questions/3498492/javascriptvoid0-vs-return-false-vs-preventdefault
-http://lea.verou.me/2011/05/change-url-hash-without-page-jump/
+// Back to Home link
+anchor[5].addEventListener('click', backToHome, false);
+
+/* ERROR: "_gaq is not defined"
+// Analytics: LinkedIn clicado
+anchor[0].addEventListener('click', _gaq.push(['_trackEvent','LinkedIn','clickado']), false);
+// Analytics: Email clicado
+anchor[1].addEventListener('click', _gaq.push(['_trackEvent','Email','clickado']), false);
+// Analytics: About me clicado
+anchor[2].addEventListener('click', _gaq.push(['_trackEvent','About-me','clickado']), false);
+// Analytics: Language clicado
+//.addEventListener('click', _gaq.push(['_trackEvent','Language','clickado']), false);
 */
-
-
-//about_text.addEventListener('click', internalURL, false);
-
-// Analytics LinkedIn clicado
-anchor[0].addEventListener(
-  'click',
-  _gaq.push([‘_trackEvent‘,‘LinkedIn’,’clickado’]),
-  false);
