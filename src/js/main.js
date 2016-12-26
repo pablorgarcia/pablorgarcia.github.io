527816a8fe6f7fd895bd8ae0330ade690c06ae43
PableraShow <pablo.ramon.garcia.fernandez@gmail.com>
2016-12-26 09:48:55 -0200

    different image version added

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,22 @@
 'use strict';
 
 var bgJS = document.createElement('script');
-bgJS.setAttribute('src', 'src/js/currentBackground.js');
-
 var asyncJS = document.createElement('script');
-asyncJS.setAttribute('src', 'src/js/asyncCSS.js');
-asyncJS.setAttribute('async', '');
-
 var cssFonts = document.createElement('link');
-cssFonts.rel = 'stylesheet';
-cssFonts.href = 'src/css/fonts.css';
-
-var cssImage = document.createElement('link');
-cssImage.rel = 'stylesheet';
-cssImage.href = 'src/css/image-profile.css';
-
-
 var userAgent = navigator.userAgent.toLowerCase();
 var jsCurrentDate = document.getElementsByTagName('script');
 var head = document.getElementsByTagName('head')[0];
 var body = document.getElementsByTagName('body')[0];
-var aLinkedIn = document.getElementsByTagName('a')[0];
-var aMail = document.getElementsByTagName('a')[1];
+var aMail = document.getElementsByTagName('a')[0];
+var aLinkedIn = document.getElementsByTagName('a')[1];
+var aTwitter = document.getElementsByTagName('a')[2];
+
+// adding the propieties to the variabels
+bgJS.setAttribute('src', 'src/js/currentBackground.js');
+asyncJS.setAttribute('src', 'src/js/asyncCSS.js');
+asyncJS.setAttribute('async', '');
+cssFonts.rel = 'stylesheet';
+cssFonts.href = 'src/css/fonts.css';
 
 // Preguntamos cual es el cliente del navegador
 if (userAgent.search(/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|webos|wos)/i) !== -1) {
@@ -34,6 +29,5 @@ if (userAgent.search(/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone
   // Cargamos con JS los siguientes CSS
   body.appendChild(bgJS);
   head.appendChild(cssFonts);
-  head.appendChild(cssImage);
 
 }
