ebcfd73ad51595e5f8648c174e0b3ff05b414020
Pablo García <heypablete@gmail.com>
2018-04-08 12:05:02 +0200

    changing texts

---
+++ javascripts/translate.js
@@ -0,0 +1,17 @@
+
+var dict = {
+  en: {
+    extraInfo: "@ironhack student",
+    main: {
+      1: "Good Morning",
+      2: "I'm software developer and entrepreneur of PatioMonreal.com",
+      3: ""
+    }
+  },
+  es: {
+    head: "..."
+  },
+  pt: {
+    head: "..."
+  }
+}
