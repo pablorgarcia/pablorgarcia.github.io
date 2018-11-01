6e6fc024b2adc257eb05e00b6328c3c6f3f55145
Pablo García <heypablete@gmail.com>
2018-11-01 13:40:14 +0100

    delete main.js

--- js/main.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("👋 HELLO PARTNET. Feel free to fork it on https://github.com/pablorgarcia/pablorgarcia.github.io :)");
-  console.log("0");
-});
