f8708baae42810962c3a0855ee2d9434dd6a8cef
Pablo García <heypablete@gmail.com>
2018-12-14 17:46:40 +0100

    responsive size updated, main js first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Getting data info from the browser
+  const date = new Date();
+  const currentMonth = date.getMonth();
+  const currentYear = date.getFullYear();
+  const currentHour = date.getHours();
+
+  // Setting the general styles, at-dat or at-night 
+  const body = document.getElementsByTagName('body')[0];
+
+  // add at-day or at-night class depending on the current hour
+  (currentHour >= 8 && currentHour < 21) ? body.classList.add('at-day') : body.classList.add('at-night');
+
+  // Mount for time tag
+  // Mounts set
+  const month = ['January', 'February', 'March', 'April', 'March', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  // Format the string for display it
+  let space = '';
+  currentMonth < 10 ? space = '-0' : space = '-';
+  const currentDate = `${month[currentMonth]} ${currentYear}`;
+  const attDate = `${currentYear}${space}${currentMonth+1}`;
+
+  // Adding the attribute and the text on <time> element
+  const time = document.getElementsByTagName('time')[0];
+  time.setAttribute('datatime', attDate);
+  time.textContent = currentDate;
+
+});
+
+console.log("👋 HELLO PARTNET. Feel free to fork it on https://github.com/pablorgarcia/pablorgarcia.github.io :)");
