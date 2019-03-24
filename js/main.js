6c99439cd7a54d29eeb2e9ce76556e5f2316f3b8
Pablo García <heypablete@gmail.com>
2019-03-25 00:46:40 +0100

    popup title attribute on process

--- js/main.js
+++ js/main.js
@@ -30,6 +30,41 @@ document.addEventListener('DOMContentLoaded', () => {
   time.setAttribute('datatime', attDate);
   time.textContent = currentDate;
 
+  /* Show a popup div with the title atributte text */
+  const popUp = {
+    tagElement: 'div',
+    tagClass: 'pop-up',
+  }
+
+  const createPopUp = (eventTarget) => {
+    const popUpElement = document.createElement(popUp.tagElement);
+    popUpElement.setAttribute('class', popUp.tagClass);
+    const popUpText = document.createTextNode(eventTarget.title);
+    popUpElement.appendChild(popUpText);
+// https://developer.mozilla.org/es/docs/Web/API/Node/insertarAntes
+//    document.body.insertBefore(popUpElement, eventTarget); 
+    console.log('new popup element', popUpElement);
+  }
+
+  // Listen to title attribute clicks
+  document.addEventListener('click', (event) => {
+    let clickCounter = 0;
+
+    if (event.target.hasAttribute('title')) {
+      if (clickCounter != 0) {
+        console.log('ya hay un popup creado, hay que borrarlo', clickCounter);
+        clickCounter = 0;
+      }
+      clickCounter = 1;
+      createPopUp(event.target);
+    } else {
+      console.log('has clickado fuera, hay que borrar el popup', clickCounter);
+      clickCounter = 0;
+    };
+
+  }, false);
+
+
 });
 
 console.log("👋 HELLO PARTNET. Feel free to fork it on https://github.com/pablorgarcia/pablorgarcia.github.io :)");
