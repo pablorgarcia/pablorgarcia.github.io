16e26f73e1bb26dc975d7a0c1c9ccea4f379f72e
Pablo García <heypablete@gmail.com>
2019-03-28 00:25:25 +0100

    popup for pre tags created but not minified

--- js/main.js
+++ js/main.js
@@ -30,40 +30,35 @@ document.addEventListener('DOMContentLoaded', () => {
   time.setAttribute('datatime', attDate);
   time.textContent = currentDate;
 
-  /* Show a popup div with the title atributte text */
-  const popUp = {
-    tagElement: 'div',
-    tagClass: 'pop-up',
-  }
 
-  const createPopUp = (eventTarget) => {
-    const popUpElement = document.createElement(popUp.tagElement);
-    popUpElement.setAttribute('class', popUp.tagClass);
+  /* Show and hide the pop up div */
+  // Get the pop up div
+  const popUpElement = document.getElementById('pop-up');
+
+  const showPopUp = (eventTarget) => {
     const popUpText = document.createTextNode(eventTarget.title);
     popUpElement.appendChild(popUpText);
-// https://developer.mozilla.org/es/docs/Web/API/Node/insertarAntes
-//    document.body.insertBefore(popUpElement, eventTarget); 
-    console.log('new popup element', popUpElement);
-  }
+    popUpElement.setAttribute('class', 'pop-up pop-up-active');
+  };
+
+  const hidePopUp = () => {
+    popUpElement.setAttribute('class', 'pop-up pop-up-disabled');
+    popUpElement.textContent = '';
+  };
 
   // Listen to title attribute clicks
   document.addEventListener('click', (event) => {
-    let clickCounter = 0;
-
-    if (event.target.hasAttribute('title')) {
-      if (clickCounter != 0) {
-        console.log('ya hay un popup creado, hay que borrarlo', clickCounter);
-        clickCounter = 0;
-      }
-      clickCounter = 1;
-      createPopUp(event.target);
+    if (!event.target.hasAttribute('title')) {
+      hidePopUp();
     } else {
-      console.log('has clickado fuera, hay que borrar el popup', clickCounter);
-      clickCounter = 0;
-    };
+      if (popUpElement.textContent != '') {
+        hidePopUp();
+      } else {
+        showPopUp(event.target);
+      }
+    }
 
   }, false);
-
 
 });
 
