5dd08424dc666b30853b892b7dc6ef61969be2b2
Pablo García <heypablete@gmail.com>
2019-04-06 16:59:50 +0200

    path obj added

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,21 @@ const minifyJS = require('gulp-minify');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 
+const path = {
+  js: 'app/js/*.js',
+  css: 'app/css/main.css'
+};
+
 // JS main minified
 gulp.task('minify-js', () => {
-  return gulp.src('app/js/*.js')
+  return gulp.src(path.js)
     .pipe(minifyJS())
     .pipe(gulp.dest('src/js'));
 });
 
 // CSS main minified
 gulp.task('minify-css', () => {
-  return gulp.src('app/css/main.css')
+  return gulp.src(path.css)
     .pipe(cleanCSS())
     .pipe(rename('main.min.css')) 
     .pipe(gulp.dest('src/css'));
@@ -20,6 +25,6 @@ gulp.task('minify-css', () => {
 
 // Listening to changes
 gulp.task('default', () => {
-  gulp.watch('app/js/*.js', gulp.series('minify-js'));
-  gulp.watch('app/css/main.css', gulp.series('minify-css'));
+  gulp.watch(path.js, gulp.series('minify-js'));
+  gulp.watch(path.css, gulp.series('minify-css'));
 });
